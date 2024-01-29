bf43e013292b593d8df898776d6482835d982225
Elijah A <elijahatkins911@yahoo.com>
2024-01-28 22:28:14 -0600

    Final commit: Demo and presentation branch state

---
+++ testCases/doubleloop.py
@@ -0,0 +1,11 @@
+def main(n: int):
+    for i in range(n):
+        for j in range(n):
+            print("Hello World!")
+        print("How are you?")
+
+
+
+
+
+
---
+++ testCases/tripleloop.py
@@ -0,0 +1,8 @@
+def main(n: list):
+    y = 1
+    for i in range(len(n)):
+        for j in range(len(n)):
+            for x in range(len(n)):
+                print("Hello World")
+                y *= x
+
